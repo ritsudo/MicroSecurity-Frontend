a81a076cfd569ffced1c9678ad10fa3deaf346d5
ritsudo <minato-namikaje@yandex.ru>
2024-03-25 22:19:55 +0300

    feat: add clear button, add error message

--- src/app/timestamp/timestamp.component.ts
+++ src/app/timestamp/timestamp.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { TimestampService } from '../timestamp.service';
 import { CommonModule } from '@angular/common';
-import { TimestampObject } from '../dto/timestamp';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-timestamp',
   standalone: true,
   imports: [CommonModule],
+  providers: [DatePipe],
   templateUrl: './timestamp.component.html',
   styleUrl: './timestamp.component.css'
 })
@@ -14,14 +15,36 @@ import { TimestampObject } from '../dto/timestamp';
 export class TimestampComponent {
     timestampStrings: string[] = []
     buttonStatus: number = 0;
-    currentDateTime: Date = new Date();
+    currentDateTime: string = "";
+    prevControllerDateTime: string = "";
+    controllerDateTime: string = "";
+
+    isErrorVisible: boolean = true;
 
     constructor(
-      private timestampService: TimestampService
+      private timestampService: TimestampService,
+      private datePipe: DatePipe
     ) {}
 
     updateDateTime() {
-      this.currentDateTime = new Date();
+      this.currentDateTime = this.datePipe.transform(new Date(), 'yyyy-MM-dd HH:mm:ss') || '';
+      this.timestampService.getTimestamp().subscribe(timestamp => {
+          this.controllerDateTime = this.datePipe.transform(timestamp.timestamp, 'yyyy-MM-dd HH:mm:ss', 'UTC') || '';
+      });
+      if (this.controllerDateTime == this.prevControllerDateTime) {
+        this.isErrorVisible = true;
+      } else {
+        this.isErrorVisible = false;
+      }
+      this.prevControllerDateTime = this.controllerDateTime;
+    }
+
+    onClick(): void {
+      this.timestampStrings = [];
+    }
+
+    transformDate(dateString: string): string {
+      return this.datePipe.transform(dateString, 'yyyy-MM-dd HH:mm:ss', 'UTC') || '';
     }
 
     ngOnInit(): void {
@@ -35,7 +58,7 @@ export class TimestampComponent {
         if (raw.raw == 1) {
           if (this.buttonStatus == 0) {
             this.timestampService.getTimestamp().subscribe(timestamp => {
-              this.timestampStrings.push(timestamp.timestamp + " - нажатие кнопки ");
+              this.timestampStrings.push(this.transformDate(timestamp.timestamp) + " - нажатие кнопки ");
             });
             this.buttonStatus = 1;
           }
@@ -43,7 +66,7 @@ export class TimestampComponent {
         if (raw.raw == 0) {
           if (this.buttonStatus == 1) {
             this.timestampService.getTimestamp().subscribe(timestamp => {
-              this.timestampStrings.push(timestamp.timestamp + " - кнопка отпущена ");
+              this.timestampStrings.push(this.transformDate(timestamp.timestamp) + " - кнопка отпущена ");
             });
             this.buttonStatus = 0;
           }
